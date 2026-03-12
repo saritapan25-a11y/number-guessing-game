3eaef672212b5f93c3e7294cad6014b4e2c21e8a
Sarita Pandey (External) <sarita.pan.25@gmail.com>
2026-03-11 22:34:33 -0700

    added comment for readline module and function

--- number-guess-game.js
+++ number-guess-game.js
@@ -1,13 +1,17 @@
-// Use of readline module to take input
+// Use of readline module which allows to take input from the terminal.
+
 const readline = require("readline");
 
+// Create an interface
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+  input: process.stdin, // Reads user input from the terminal 
+  output: process.stdout // prints output to the terminal
 });
 
+//Generate Random number 
 const secretNumber = Math.floor(Math.random() * 10) + 1;
 
+// funtion to ask for a guess
 function askGuess() {
   rl.question("\n Guess a number between 1 and 10: ", function (guess) {
     if (parseInt(guess) === secretNumber) {
